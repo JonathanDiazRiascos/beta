cbbfaa7b47aaad423be361475ceaff5270bc239f
JonathanDiazRiascos <jonathan-diaz_95@outlook.es>
2024-09-20 12:12:17 -0500

    Endpoint: Database: connection

---
+++ api/config/db_connection.php
@@ -0,0 +1,36 @@
+<?php
+/*
+    $number1 = 20;
+    $number2 = 30;
+
+    $addition = $number1 + $number2;
+
+    echo $addition;
+    */
+    /*
+    PostgresSQL Database connection
+    Developer: Jonathan Diaz Riascos
+    */
+    $host       = "localhost";
+    $username   = "postgres";
+    $password   = "unicesmag";
+    $dbname     = "beta";
+    $port       = "5432";
+
+    $data_connection = "
+      host      = $host
+      port      = $port
+      dbname    = $dbname
+      user      = $username
+      password  = $password
+    ";
+    $conn = pg_connect($data_connection);
+
+    if (!$conn) {
+        die("Connection failed".preg_last_error());
+    }else {
+        echo "Connected successfully";
+    }
+
+    pg_close($conn);
+?>
